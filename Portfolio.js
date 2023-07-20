d6e6371d1ef62bf2789e5449f3a603840095cbb8
Jacob Schoff <jschoff155@gmail.com>
2023-07-19 22:36:10 -0500

    Mobile and desktop format completed. Next: Format top nav bar

---
+++ Portfolio.js
@@ -0,0 +1,13 @@
+function showAttribution() {
+    document.getElementById("attributionArea").style.display="block";
+    document.getElementById("attributionareaclosebtn").style.display="block";
+    document.getElementById("attributionareabtn").style.display="none";
+}
+function closeAttribution() {
+    document.getElementById("attributionArea").style.display="none";
+    document.getElementById("attributionareaclosebtn").style.display="none";
+    document.getElementById("attributionareabtn").style.display="block";
+}
+function menuClick() {
+    document.getElementById("navMenu").style.display="block";
+}
